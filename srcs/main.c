3e4e763bdcda1c5fc62718441ddb7b06cab2ed1c
河田鷹介 <benefium@gmail.com>
2023-08-08 01:37:55 +0900

    ft_puts.c -> bsq_putstr.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: corvvs <corvvs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 02:58:38 by louisnop          #+#    #+#             */
-/*   Updated: 2023/08/07 22:51:23 by corvvs           ###   ########.fr       */
+/*   Updated: 2023/08/08 01:36:57 by corvvs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		main(int argc, char *argv[]) {
 	// [using arguments]
 	for (int i = 1; i < argc; ++i) {
 		if (1 < i) {
-			printf("\n");
+			ft_putstr_fd(STDIN_FILENO, "\n");
 		}
 		int ifd = open(argv[i], O_RDONLY);
 		run_bsq_session(ifd);
