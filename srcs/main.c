98a8e0041eaef1ecc1c9af4ac0bce1532467d21d
河田鷹介 <benefium@gmail.com>
2023-08-08 19:19:00 +0900

    mainの分割方法を変更

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: corvvs <corvvs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 02:58:38 by louisnop          #+#    #+#             */
-/*   Updated: 2023/08/08 16:11:21 by corvvs           ###   ########.fr       */
+/*   Updated: 2023/08/08 19:13:45 by corvvs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,26 @@ static void	bsq_for_stdin(void) {
 	bsq_run_session(STDIN_FILENO);
 }
 
-static void	bsq_for_arguments(int argc, char **argv) {
-	for (int i = 0; i < argc; ++i) {
-		if (0 < i) {
-			ft_putstr_fd(STDIN_FILENO, "\n");
-		}
-
-		int ifd = open(argv[i], O_RDONLY);
-		DEBUGINFO("ping session for \"%s\"", argv[i]);
-		bsq_run_session(ifd);
-		if (ifd < 0) {
-			continue;
-		}
-		close(ifd);
+static void	bsq_for_file(const char* filename) {
+	int ifd = open(filename, O_RDONLY);
+	DEBUGINFO("ping session for \"%s\"", filename);
+	bsq_run_session(ifd);
+	if (ifd < 0) {
+		return ;
 	}
+	close(ifd);
 }
 
 int		main(int argc, char *argv[]) {
 	if (argc < 2) {
 		bsq_for_stdin();
-	} else {
-		bsq_for_arguments(argc - 1, argv + 1);
+		return (0);
+	}
+	for (int i = 1; i < argc; ++i) {
+		if (1 < i) {
+			ft_putstr_fd(STDIN_FILENO, "\n");
+		}
+		bsq_for_file(argv[i]);
 	}
 	return (0);
 }
