4746d628fe7d42289d1a3380a9b3bdb4e56ab4c9
河田鷹介 <benefium@gmail.com>
2023-08-08 19:06:10 +0900

    bugfix: open失敗時だけcloseしている

--- srcs/main.c
+++ srcs/main.c
@@ -6,29 +6,38 @@
 /*   By: corvvs <corvvs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 02:58:38 by louisnop          #+#    #+#             */
-/*   Updated: 2023/08/08 11:21:19 by corvvs           ###   ########.fr       */
+/*   Updated: 2023/08/08 16:11:21 by corvvs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-int		main(int argc, char *argv[]) {
-	if (argc < 2) {
-		// [from stdin]
-		bsq_run_session(STDIN_FILENO);
-		return (0);
-	}
-	// [using arguments]
-	for (int i = 1; i < argc; ++i) {
-		if (1 < i) {
+static void	bsq_for_stdin(void) {
+	DEBUGINFO("ping session for %s", "STDIN");
+	bsq_run_session(STDIN_FILENO);
+}
+
+static void	bsq_for_arguments(int argc, char **argv) {
+	for (int i = 0; i < argc; ++i) {
+		if (0 < i) {
 			ft_putstr_fd(STDIN_FILENO, "\n");
 		}
-		DEBUGINFO("ping session for \"%s\"", argv[i]);
+
 		int ifd = open(argv[i], O_RDONLY);
+		DEBUGINFO("ping session for \"%s\"", argv[i]);
 		bsq_run_session(ifd);
 		if (ifd < 0) {
-			close(ifd);
+			continue;
 		}
+		close(ifd);
+	}
+}
+
+int		main(int argc, char *argv[]) {
+	if (argc < 2) {
+		bsq_for_stdin();
+	} else {
+		bsq_for_arguments(argc - 1, argv + 1);
 	}
 	return (0);
 }
